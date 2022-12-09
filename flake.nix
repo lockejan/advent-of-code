8b7214fcbf6462608aa423ad05e3cfcd922660e2
Jan Schmitt <git@smittie.de>
2022-12-09 16:56:18 +0100

    chore: switch to lua5.1

--- flake.nix
+++ flake.nix
@@ -1,19 +1,21 @@
 {
   description = "Runtime environments for advent-of-code";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs-22-05.url = "github:NixOS/nixpkgs/nixos-22.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-22-05, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        pkgs-22-05 = nixpkgs-22-05.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = [ pkgs.bashInteractive ];
           buildInputs = [
-            pkgs.lua
-            pkgs.lua52Packages.busted
+            pkgs-22-05.lua5_1
+            pkgs.lua51Packages.busted
             # pkgs.ghc
             # pkgs.clojure
             # pkgs.python311
